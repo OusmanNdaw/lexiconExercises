93785fe4887365bb6fcdd117df7fda1aee709ea6
OusmanNdaw <105175511+OusmanNdaw@users.noreply.github.com>
2022-05-10 16:47:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+class personalRegister
+{
+    {
+//metod hämtaInPersonalUppgifter()
+Hämta in personalens namn:Vad är ditt namn?
+ -> lagra personal namn
+Hämta in personalens efternamn:Vad är ditt efternamn?
+ ->   lagra personal efternamn
+Hämta in personalens lön:Vad har du för lön just nu?
+  lagra personal lön
+    }
+
+//metod lagraPersonalUppgifter()
+
+//metod skrivaUtpersonalUppgifter()
+skriv ut personalens namn:Vad är ditt namn? -> namn
+skriv ut personalens efternamn:Vad är ditt efternamn?  -> efternamn
+skriv ut personalens lön:Vad har du för lön just nu? -> lön
+}
+//skrivaUtpersonalLön()
+
+Få tillfråga om en personalslön och svara
+
+// Vad har personal A som lön?
+Skriva ut personal löneuppgifter
+{
+personal "namn" + "efternamn" har i "lön"
+}
+
